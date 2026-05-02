6e152506859e127d39454f0fa79a895b2d3b9c01
Miguel Ferreiro <ferreirogarciamiguel@gmail.com>
2026-05-02 22:58:25 +0200

    Mover o token Mapbox a secret de GitHub Actions
    Inxecta o token desde MAPBOX_TOKEN durante o despregue en lugar de
    hardcodealo en config.js. Cambio a build_type=workflow en Pages.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
     style: 'mapbox://styles/miguel-ferr/cmf4jek5100hv01plgk8pe1lg',
-    accessToken: 'pk.eyJ1IjoibWlndWVsLWZlcnIiLCJhIjoiY21pbnI4dWhmMGVjaTNncjEwcTl5NjVmeiJ9.6vtKRrzI4kxzFDiHNFOgpg',
+    accessToken: '__MAPBOX_TOKEN__',
     
     // Capas que se ocultan automáticamente por defecto
     hiddenLayers: ['USOURENSE', 'XEOURENSE', 'RELIEVE'],
